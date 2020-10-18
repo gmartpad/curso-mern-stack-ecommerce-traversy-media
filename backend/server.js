6a53dc70ef382158706969ff70176d477ef0adce
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-10-18 01:21:43 -0300

    Environment Variables

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
+
+//inicializa o .env
+dotenv.config()
 
 //initializar o express
 const app = express()
 
 //porta do servidor
-const port = 5000
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('API tá rodando...')
@@ -21,4 +25,4 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product);
 })
 
-app.listen(port, console.log(`Servidor rodando na porta ${port}`))
+app.listen(PORT, console.log(`Servidor rodando no modo de ${process.env.NODE_ENV} na porta ${process.env.PORT}`))
